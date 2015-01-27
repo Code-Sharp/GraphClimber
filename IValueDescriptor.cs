ad6f03776ca8a2e8a79c4a449923bcecab8605c5
Elad Zelingher <dark_leizar@walla.com>
2015-01-27 16:33:20 +0200

    Introducing IReadValueDescriptor, IWriteValueDescriptor.

--- IValueDescriptor.cs
+++ IValueDescriptor.cs
@@ -1,31 +1,14 @@
 namespace GraphClimber
 {
-
-
     /// <summary>
     /// Represents a description for a value of type
-    /// <typeparamref name="TField"/>
     /// </summary>
-    /// <typeparam name="TField"></typeparam>
-    public interface IValueDescriptor<TField>
+    public interface IValueDescriptor
     {
-
         /// <summary>
         /// Gets the state member that owns the value
         /// </summary>
         IStateMember StateMember { get; }
-
-        /// <summary>
-        /// Sets the field to a new value
-        /// </summary>
-        /// <param name="value"></param>
-        void Set(TField value);
-
-        /// <summary>
-        /// Gets the current value of the field
-        /// </summary>
-        /// <returns></returns>
-        TField Get();
 
         /// <summary>
         /// Gets the owner of the field
@@ -36,6 +19,57 @@ namespace GraphClimber
         /// Climbs on the (current) value that found on the owner.
         /// </summary>
         void Climb();
+    }
 
+    /// <remarks>
+    /// Allows processing state member values based on their RuntimeType:
+    /// Used mostly for serialization.
+    /// </remarks>
+    public interface IReadValueDescriptor<out TRuntime> : IValueDescriptor
+    {
+        /// <summary>
+        /// Gets the current value of the field
+        /// </summary>
+        /// <returns></returns>
+        TRuntime Get();         
+    }
+
+    /// <remarks>
+    /// Allows processing state member values based on their static type:
+    /// Used mostly for deserialization.
+    /// </remarks>
+    public interface IWriteValueDescriptor<in TField> : IValueDescriptor
+    {
+        /// <summary>
+        /// Sets the field to a new value
+        /// </summary>
+        /// <param name="value"></param>
+        void Set(TField value);         
+    }
+
+    /// <remarks>
+    /// Allows processing state member values based on both static and runtime types.
+    /// </remarks>
+    /// <example>
+    /// IReadWriteValueDescriptor{IEnumerable{string}, ICollection{string}} represents
+    /// state members that have static type that is at most IEnumerable{string}, but 
+    /// runtime type that is at least ICollection{string}.
+    /// </example>
+    public interface IReadWriteValueDescriptor<out TField, in TRuntime> :
+        IReadValueDescriptor<TField>, IWriteValueDescriptor<TRuntime>
+    {
+    }
+
+    /// <remarks>
+    /// Allows processing state member values based on both static and runtime types.
+    /// </remarks>
+    /// <example>
+    /// IReadWriteValueDescriptor{IEnumerable{string}, ICollection{string}} represents
+    /// state members that have static type that is at most IEnumerable{string}, but 
+    /// runtime type that is at least ICollection{string}.
+    /// </example>
+    public interface IReadWriteValueDescriptor<TField> :
+        IReadWriteValueDescriptor<TField, TField>
+    {
     }
 }
--- Program.cs
+++ Program.cs
@@ -17,16 +17,18 @@ namespace GraphClimber
 
     }
 
-    public class MyInheritedProcessor : IInheritedProcessor<IAsyncResult>, IProcessor<int[]>, IRevisitedFilter
+    public class MyInheritedProcessor : IWriteProcessor<IAsyncResult>, IProcessor<int[]>, IRevisitedFilter
     {
-
-        public void Process<TReal>(TReal value, IValueDescriptor<IAsyncResult> descriptor) where TReal : IAsyncResult
+        public void ProcessForWrite(IWriteValueDescriptor<IAsyncResult> descriptor)
         {
+            // Sets all fields that are assignable from IAsyncResult to null.
+            // (i.e: all fields that have static type object or IAsyncResult)
             descriptor.Set((IAsyncResult)null);
         }
 
-        public void Process(int[] value, IValueDescriptor<int[]> descriptor)
+        public void ProcessForReadWrite(IReadWriteValueDescriptor<int[]> descriptor)
         {
+            int[] value = descriptor.Get();
             value[0] = 4;
             descriptor.Set(value);
         }
@@ -34,6 +36,16 @@ namespace GraphClimber
         public bool Visited(object obj)
         {
             return false;
+        }
+    }
+
+    public class MyInheritedProcessor2 : IReadWriteProcessor<IAsyncResult>
+    {
+        public void ProcessForReadWrite(IReadWriteValueDescriptor<IAsyncResult> descriptor)
+        {
+            // Sets all field that are assignable from IAsyncResult and are
+            // actually currently from type IAsyncResult, to null.
+            descriptor.Set((IAsyncResult)null);
         }
     }
 }
