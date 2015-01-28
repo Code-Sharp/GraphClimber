1387030abb9a03e57b1370b90af81c32a200629d
Shani Elharrar <shani.elha@gmail.com>
2015-01-28 14:42:48 +0200

    Added ProcessorMethodAttribute

---
+++ src/GraphClimber/ProcessorMethodAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GraphClimber
+{
+    /// <summary>
+    /// This attribute annotates a method inside a processor 
+    /// as a processor method, Which means that the method can be called
+    /// by the graph climber if it is found as the most suitable method to call.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ProcessorMethodAttribute : Attribute
+    {
+        public ProcessorMethodAttribute()
+        {
+            Precedence = 100;
+        }
+
+        /// <summary>
+        /// Gets or sets the precedence of the method 
+        /// annotated by this attribute. 
+        /// 
+        /// Defaults to 100.
+        /// </summary>
+        public int Precedence { get; set; }
+    }
+
+}
