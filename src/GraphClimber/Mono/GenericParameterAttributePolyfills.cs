ece481d3d9182502043095a028fabaa073d8c398
Shani Elharrar <shani.elha@gmail.com>
2015-01-28 09:19:59 +0200

    Added GenericParameterAttributes Pollyfills (mono)

---
+++ src/GraphClimber/Mono/GenericParameterAttributePolyfills.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace GraphClimber
+{
+    public static class GenericParameterAttributePolyfills
+    {
+
+        public static bool HasFlag(this GenericParameterAttributes value, GenericParameterAttributes flag)
+        {
+            return (value & flag) == flag;
+        }
+
+    }
+}
