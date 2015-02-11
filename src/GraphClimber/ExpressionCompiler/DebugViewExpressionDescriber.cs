6f89299fd0f1181d581e9703fa94da7806592b0e
Shani Elharrar <shani.elha@gmail.com>
2015-02-11 10:18:28 +0200

    Added Documentation to ExpressionCompiler classes.

--- src/GraphClimber/ExpressionCompiler/DebugViewExpressionDescriber.cs
+++ src/GraphClimber/ExpressionCompiler/DebugViewExpressionDescriber.cs
@@ -3,12 +3,22 @@ using System.Reflection;
 
 namespace GraphClimber.ExpressionCompiler
 {
+    /// <summary>
+    /// Implementation of <see cref="IExpressionDescriber"/>
+    /// using the property "DebugView" of the expressions.
+    /// 
+    /// This may not work if microsoft/mono changes the implementation
+    /// of "DebugView".
+    /// </summary>
     public class DebugViewExpressionDescriber : IExpressionDescriber
     {
         public static readonly IExpressionDescriber Empty = new DebugViewExpressionDescriber();
 
         private static readonly PropertyInfo _debugView = typeof(Expression).GetProperty("DebugView", BindingFlags.NonPublic | BindingFlags.Instance);
 
+        /// <summary>
+        /// Private C'tor, Please use <see cref="Empty"/> property to get the instance of <see cref="DebugViewExpressionDescriber"/>.
+        /// </summary>
         private DebugViewExpressionDescriber()
         {
             
--- src/GraphClimber/ExpressionCompiler/IExpressionDescriber.cs
+++ src/GraphClimber/ExpressionCompiler/IExpressionDescriber.cs
@@ -2,6 +2,11 @@ using System.Linq.Expressions;
 
 namespace GraphClimber.ExpressionCompiler
 {
+    /// <summary>
+    /// Represents an object that can transform expressions to their describing strings.
+    /// 
+    /// E.g, Transform expression to C# code.
+    /// </summary>
     public interface IExpressionDescriber
     {
 
