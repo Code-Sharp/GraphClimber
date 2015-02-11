80a96b68327951b305785340f23e41567d0ad3f5
Elad Zelingher <dark_leizar@walla.com>
2015-02-11 16:27:22 +0200

    Interface extractor sample

---
+++ src/GraphClimber/Examples/Extractor/Extractor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace GraphClimber.Examples
+{
+    public class Extractor<T>
+    {
+        private readonly SlowGraphClimber<ExtractorProcessor<T>> _climber;
+
+        public Extractor()
+        {
+            _climber =
+                new SlowGraphClimber<ExtractorProcessor<T>>(new ReflectionPropertyStateMemberProvider());
+        }
+
+        public IEnumerable<T> Extract(object value)
+        {
+            ExtractorProcessor<T> processor = new ExtractorProcessor<T>();
+
+            _climber.Route(value, processor, false);
+
+            return processor.Result;
+        }
+    }
+}
---
+++ src/GraphClimber/Examples/Extractor/ExtractorProcessor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace GraphClimber.Examples
+{
+    internal class ExtractorProcessor<T> : INullProcessor
+    {
+        private readonly List<T> _gathered = new List<T>();
+
+        public IEnumerable<T> Result
+        {
+            get
+            {
+                return _gathered;
+            }
+        }
+
+        [ProcessorMethod(Precedence = 1)]
+        public virtual void Process<TRuntime>
+            (IReadOnlyValueDescriptor<TRuntime> descriptor)
+            where TRuntime : T
+        {
+            T runtime = descriptor.Get();
+            _gathered.Add(runtime);
+        }
+
+        [ProcessorMethod(Precedence = 2)]
+        public void ProcessGeneric<TRuntime>
+            (IReadOnlyValueDescriptor<TRuntime> descriptor)
+        {
+            descriptor.Climb();
+        }
+
+        public void ProcessNull<TField>(IWriteOnlyValueDescriptor<TField> descriptor)
+        {
+            // Don't climb
+        }
+    }
+}
