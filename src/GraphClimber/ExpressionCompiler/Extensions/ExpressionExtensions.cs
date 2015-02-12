a390997d47d69c8a42dd8802070d643a6fc7a3ac
Shani Elharrar <shani.elha@gmail.com>
2015-02-12 12:14:24 +0200

    Documented ExpressionExtensions.

--- src/GraphClimber/ExpressionCompiler/Extensions/ExpressionExtensions.cs
+++ src/GraphClimber/ExpressionCompiler/Extensions/ExpressionExtensions.cs
@@ -1,21 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
 
 namespace GraphClimber.ExpressionCompiler.Extensions
 {
+    /// <summary>
+    /// Provides extension methods to create
+    /// expressions easier.
+    /// </summary>
     public static class ExpressionExtensions
     {
 
+        /// <summary>
+        /// Creates an <see cref="ConstantExpression"/>
+        /// with the given value
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static Expression Constant<T>(this T value)
         {
             return Expression.Constant(value, typeof (T));
         }
 
-
+        /// <summary>
+        /// Converts the expression value to the given type
+        /// <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public static Expression Convert<T>(Expression expression)
+        {
+            return Expression.Convert(expression, typeof (T));
+        }
 
     }
 }
