ae11ce73cdda9989f8941e708ba358e4cc9586c1
Shani Elharrar <shani.elha@gmail.com>
2015-02-18 10:34:22 +0200

    Added FallbackExpressionCompiler.
    (until all problems with debug are solved)

--- src/GraphClimber/ExpressionCompiler/TrivialExpressionCompiler.cs
+++ src/GraphClimber/ExpressionCompiler/TrivialExpressionCompiler.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace GraphClimber.ExpressionCompiler
@@ -7,6 +9,36 @@ namespace GraphClimber.ExpressionCompiler
         public TDelegate Compile<TDelegate>(Expression<TDelegate> expression)
         {
             return expression.Compile();
+        }
+    }
+
+    public class FallbackExpressionCompiler : IExpressionCompiler
+    {
+        private readonly IExpressionCompiler[] _compilers;
+
+        public FallbackExpressionCompiler(params IExpressionCompiler[] compilers)
+        {
+            _compilers = compilers;
+        }
+
+        public TDelegate Compile<TDelegate>(Expression<TDelegate> expression)
+        {
+            IList<Exception> exceptions = null;
+
+            foreach (var compiler in _compilers)
+            {
+                try
+                {
+                    return compiler.Compile(expression);
+                }
+                catch (Exception e)
+                {
+                    exceptions = exceptions ?? new List<Exception>();
+                    exceptions.Add(e);
+                }
+            }
+            
+            throw new AggregateException("Could not compile expression with any of the compilers given. Exceptions are collected.", exceptions);
         }
     }
 }
