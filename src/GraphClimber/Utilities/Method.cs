9c6e5b2537bd6b2f7f775fb7f6ad595890ab122e
Shani Elharrar <shani.elha@gmail.com>
2015-02-22 14:39:07 +0200

    Making Method.Get more runtime safe

--- src/GraphClimber/Utilities/Method.cs
+++ src/GraphClimber/Utilities/Method.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace GraphClimber
 {
@@ -8,9 +9,25 @@ namespace GraphClimber
     {
         public static MethodInfo Get<T>(Expression<Action<T>> methodCall)
         {
-            MethodCallExpression callExpression = methodCall.Body as MethodCallExpression;
+            MethodCallExpression callExpression = CastExpression<MethodCallExpression>(methodCall.Body);
 
             return callExpression.Method;
+        }
+
+        private static T CastExpression<T>(Expression expression, [CallerMemberName] string callerName = null) 
+            where T : Expression
+        {
+            var retVal = expression as T;
+
+            if (retVal == null)
+            {
+                throw new Exception(
+                    string.Format(
+                        "Method {0}.{1} expects that the method body will be expression of type {2}, got {3} instead.",
+                        MethodBase.GetCurrentMethod().DeclaringType, callerName, typeof (T), expression.GetType()));
+            }
+
+            return retVal;
         }
     }
 }
